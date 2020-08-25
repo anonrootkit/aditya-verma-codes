041f49c4196c07cfc48903dbfb6f7754661ba5b3
Ankit Gupta <ankit.gup20@gmail.com>
2020-08-25 21:29:05 +0530

    Previous greater element program added

---
+++ pge.cpp
@@ -0,0 +1,43 @@
+//Previous greater element
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void findPGE(int arr[], int size);
+void printVector(vector<int>, int);
+
+int main(){
+    int size;
+    cout<<"Enter the size of array :";cin>>size;
+    int arr[size];
+    for(int i = 0; i < size; i++) cin >> arr[i];
+
+    findPGE(arr,size);
+    return 0;
+}
+
+void findPGE(int arr[], int size){
+    stack<int> s;
+    vector<int> v;
+
+    for(int i = 0 ; i < size; i++){
+        if(s.empty()) v.push_back(-1);
+        else if(s.top() > arr[i]) v.push_back(s.top());
+        else{
+            while(s.empty() && s.top() <= arr[i]) s.pop();
+
+            v.push_back(s.empty() ? -1 : s.top() > arr[i] ? s.top(): -1);
+        }
+        s.push(arr[i]);
+    }
+    printVector(v, size);
+}
+
+
+void printVector(vector<int> v, int size){
+    for(int i = 0; i < size ; i++){
+        cout << v[i] << " ";
+    }
+    cout <<"\n";
+}
